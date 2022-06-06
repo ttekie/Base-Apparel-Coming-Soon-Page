ca6f92e219561b97fe88563c3bb4c4084cc9abfa
Tesfalem Tekie <teshanna72016@gmail.com>
2022-06-06 12:23:14 -0500

    js file added

--- js/main.js
+++ js/main.js
@@ -1,2 +1,24 @@
 "use strict";
-const regEx = /\S+@\S+\.\S+/;
+let input = document.querySelector(".apparel__input");
+let form = document.querySelector(".apparel__form");
+function emailValidity(email) {
+   let regex = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+   return regex.test(email);
+}
+
+function formStyle(event) {
+   // prevent the form from submitting
+   event.preventDefault();
+   
+   const IS_VALID = emailValidity(input.value);
+
+   input.value = `${IS_VALID ? "" : input.value}`;
+   input.style.border = `2px solid ${IS_VALID ? "hsl(145, 41%, 51%)" : "hsl(0, 93%, 68%)"}`;
+   form.classList.add(`${IS_VALID ? "apparel__email__success" : "apparel__email__error"}`);   
+ 
+   setTimeout(() => {
+      input.style.border = "";
+      form.classList.remove(`${IS_VALID ? "apparel__email__success" : "apparel__email__error"}`);
+   },1500);
+}
+form.addEventListener("submit", formStyle);
